ec10e2856a0008af4c743b57e307023b19c18233
yongckim <zxcv9203@naver.com>
2022-07-23 21:22:58 +0900

    day04 done

---
+++ src/main/java/yongcheol/day04/Main.java
@@ -0,0 +1,17 @@
+package yongcheol.day04;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(
+            new String[][]{
+                {"100", "ryan", "music", "2"},
+                {"200", "apache", "math", "2"},
+                {"300", "tube", "computer", "3"},
+                {"400", "con", "computer", "4"},
+                {"500", "muzi", "music", "3"},
+                {"600", "apeach", "music", "2"}
+            }
+        ));
+    }
+}
---
+++ src/main/java/yongcheol/day04/Solution.java
@@ -0,0 +1,25 @@
+package yongcheol.day04;
+
+public class Solution {
+
+    void dfs(int idx, int depth, boolean[] used, int row, int answer, String[][] relation) {
+        if (used[idx]) {
+            return ;
+        }
+        for (int i = 0; i < depth; i++) {
+            for (relation[idx][0])
+        }
+    }
+
+    public int solution(String[][] relation) {
+        int answer = 0;
+        int col = relation.length;
+        int row = relation[0].length;
+        boolean[] used = new boolean[col];
+
+        for (int i = 0; i < col; i++) {
+            dfs(i, 1, used, row, answer, relation);
+        }
+        return answer;
+    }
+}
