f35f3e230824396f18289a7efb2a272639ac58ca
yongckim <zxcv9203@naver.com>
2022-07-31 05:57:29 +0900

    day11

---
+++ src/main/java/yongcheol/day11/Main.java
@@ -0,0 +1,9 @@
+package yongcheol.day11;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("abcde"));
+        System.out.println(new Solution().solution("qwer"));
+    }
+}
